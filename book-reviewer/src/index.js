1a65e42dcf6c9357494a13d3038a89324079d7a2
adarvin14 <amdarvin14@gmail.com>
2021-04-09 14:41:36 -0400

    update index.js

--- book-reviewer/src/index.js
+++ book-reviewer/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import { createStrore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reportWebVitals from './reportWebVitals';
+import booksReducer from './reducers/booksreducer';
+
+const store = createStore(booksReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
