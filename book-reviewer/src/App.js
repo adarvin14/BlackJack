503e4433e2e0fe84e6b26b389a7e369b11a2e74e
adarvin14 <amdarvin14@gmail.com>
2021-04-11 21:01:49 -0400

    App.js wip

--- book-reviewer/src/App.js
+++ book-reviewer/src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { getBooks } from './actions';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Index from './components/Index';
+import Form from './components/Form';
 
 class App extends Component {
+
+  //componentDidMount function
+
   render() {
     return (
       <div className="App">
