3a8d82fea707725c66e2b6f9410fb6115c3ce1c5
adarvin14 <amdarvin14@gmail.com>
2021-04-11 21:07:18 -0400

    complete actions

--- book-reviewer/src/actions/index.js
+++ book-reviewer/src/actions/index.js
@@ -1,9 +1,26 @@
 export const getBooks = () => {
     return dispatch => {
-      dispatch({ type: "LOADING" })
-      fetch("http://localhost:3001/books")
-        .then(resp => resp.json())
-        .then(books => dispatch({ type: "SET_BOOKS", books }))
+        dispatch({ type: "LOADING" })
+        fetch("http://localhost:3001/books")
+            .then(resp => resp.json())
+            .then(books => dispatch({ type: "SET_BOOKS", books }))
     }
 }
   
+export const addBook = (book, history) => {
+    return dispatch => {
+        fetch('http://localhost:3001/books', {
+            method: "POST",
+            headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ book })
+        })
+            .then(resp => resp.json())
+            .then(book => {
+            dispatch({ type: "ADD_BOOK", book })
+            history.push("/books")
+        })
+    }
+}
