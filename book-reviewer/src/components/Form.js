9f585a3f30e45b7e701d5369c02b244dae963378
adarvin14 <amdarvin14@gmail.com>
2021-04-11 20:51:53 -0400

    add functions for Form.js

--- book-reviewer/src/components/Form.js
+++ book-reviewer/src/components/Form.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react'
 
 export class Form extends Component {
+    state = {
+        title: "",
+        author: "",
+        review: "",
+    }
+
+    handleChange = e => {
+        this.setState({
+          [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+    
+        this.props.addBook(this.state, this.props.history);
+    }
+
     render() {
         return (
             <div>
