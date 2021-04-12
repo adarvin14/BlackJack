8bf25c6b283e1cc2412d1052f3956554da453498
adarvin14 <amdarvin14@gmail.com>
2021-04-11 20:54:02 -0400

    create book form

--- book-reviewer/src/components/Form.js
+++ book-reviewer/src/components/Form.js
@@ -22,7 +22,25 @@ export class Form extends Component {
     render() {
         return (
             <div>
-                Form
+                <h3>Create a Book</h3>
+                <form onSubmit={ this.handleSubmit }>
+                    <div>
+                        <label htmlFor="title">Title </label>
+                        <input type="text" id="title" name="title" value={ this.state.title } onChange={ this.handleChange } />
+                    </div>
+                    <br />
+                    <div>
+                        <label htmlFor="author">Author </label>
+                        <input type="text" id="author" name="author" value={ this.state.author } onChange={ this.handleChange } />
+                    </div>
+                    <br />
+                    <div>
+                        <p><label htmlFor="review">Review</label></p>
+                        <textarea id="review" name="review" value={ this.state.review } onChange={ this.handleChange }></textarea>
+                    </div>
+                    <br />
+                    <input type="submit" value="Create Book" />
+                </form>
             </div>
         )
     }
